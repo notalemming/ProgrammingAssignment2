e341898d6da1e9754cf66a70d02e378a142b59d8
Raymond <raymondpconnolly@gmail.com>
2015-09-24 01:37:53 -0400

    working code with minimal comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# In this script we introduce the <<- operator which can be used to assign a value to an object 
+# in an environment that is different from the current environment. Below are two functions that 
+# are used to create a special object that stores an invertible matrix and cache's its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+     
+
+     m <- NULL      
+     
+     set <- function(y) {
+    
+          
+          x <<- y                  # set the invertible matrix x to a new matrix y
+          m <<- NULL               # reset the inverse m to NULL.  m == Null is interpeted as 
+                                   # "the inverse value of invertible matrix x is NOT in cache"                                   
+     }
+     get <- function() x                                    # returns the invertible matrix x
+     setinverse <- function(inverse) m <<- inverse          # sets the inverse m to inverse
+     getinverse <- function() m                             # returns the inverse m
+     
+     # return the list containing all of the functions defined above
+     list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     
+     # retrieve the value of the inverse (m), which may possibly have been previously cached  
+     m <- x$getinverse()
+    
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     message("No cached data => Calulate inverse")     # the inverse does not exist in cache
+     data <- x$get()                                   # retrieve the invertible matrix encapsulated 
+                                                       # in object variable x
+     m <- solve(data, ...)                             # calculate the inverse of the retrieved data
+     x$setinverse(m)                                   # cache the inverse matrix
+     m                                                 # return the inverse matrix
 }
